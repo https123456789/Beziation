ebb018d5ee9bf012a1bbeab4a7005d18163cc2c6
Ben Landon <81256789+https123456789@users.noreply.github.com>
2022-08-11 14:17:31 -0700

    Converted to a module (#1)

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+from pygame import Vector2 as Vec
+from main import *
+
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+
+print(expansion("1-t", "t", 3, 1))
+
+def drawCurve(win, points, color = (255, 255, 255)):
+    for x in range(0, len(points)-1):
+        pygame.draw.line(win, color, points[x], points[x+1])
+
+b = beziur(3, [(0, 0), (75, 100), (200, 100), (250, 200)])
+while True:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            pygame.quit()
+    
+    win.fill((0, 0, 0))
+    drawCurve(win, b)
+    pygame.display.update()
